451aef4598a51790019f06a96d66b31234fbf742
kirilliberman1987 <43469460+kirilliberman1987@users.noreply.github.com>
2018-09-21 13:27:56 +0300

    Add files via upload

---
+++ src/ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+
+
+        private IItemRepository itemRepo;
+        public ItemsController(IItemRepository thisRepo = null)
+        {
+            if (thisRepo == null)
+            {
+                this.itemRepo = new EFItemRepository(); 
+            }
+            else
+            {
+                this.itemRepo = thisRepo;
+            }
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(itemRepo.Items.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisItem = itemRepo.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            itemRepo.Save(item);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisItem = itemRepo.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Item item)
+        {
+            itemRepo.Edit(item);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisItem = itemRepo.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisItem = itemRepo.Items.FirstOrDefault(items => items.ItemId == id);
+            itemRepo.Remove(thisItem);
+            return RedirectToAction("Index");
+        }
+    }
+}
